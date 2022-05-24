1f923c0307614b752817f7d8450dc57cd8fa648e
Allan Williams Musembya Kluz <engallanmusembya@gmail.com>
2022-05-24 17:08:55 +0300

    Update TellerNotify.py

--- models/TellerNotify.py
+++ models/TellerNotify.py
@@ -16,7 +16,7 @@ class TellerNotify(models.Model):
                     obj.spot_check_date = res.created_on
                     obj.actual_no = obj.actual_no + 1
 
-        template_id = self.env.ref('spot_check.email_template_create_teller_request_consent').id
+        template_id = self.env.ref('spot_check.email_template_create_teller_request').id
         template =  self.env['mail.template'].browse(template_id)
         template.send_mail(res.id,force_send=True)
         return res
