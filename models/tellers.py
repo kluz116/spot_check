b219c9f00f68942a514e3d1c776691991618e09b
Allan Williams Musembya Kluz <engallanmusembya@gmail.com>
2022-07-02 17:16:53 +0300

    xx

--- models/tellers.py
+++ models/tellers.py
@@ -373,7 +373,7 @@ class Tellers(models.Model):
     def _checkbranchspotcheck(self):
         pending_conf = self.env['spot_check.teller'].search([('state', 'in', ['ongoing'])])
         for res in pending_conf:
-            if  res.branch_id.id == self.branch_id.id and res.state =='ongoing' and res.id is not self.id:
+            if  res.branch_id.id == self.branch_id.id and res.state =='ongoing' and res.unique_field is not self.unique_field:
                 raise exceptions.ValidationError(f"Hello {res.partner_id.name},  {res.branch_id.branch_name} still has a pending spot check confirmantion  of {res.grand_total_ugx:,.2f} UGX created on {res.created_on} by {res.created_by.name} . Kindly inform Teller {res.teller_id.name} to cosent all the spot checks before you proceed. For any more assistance please contact operations ")
     
     '''
