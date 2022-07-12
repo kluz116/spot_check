d78a529665ca56fecbc6554da49aa0b6f784ad88
Allan Williams Musembya <engallanmusembya@gmail.com>
2022-07-12 13:37:47 +0300

    new updates
    new updates

--- models/tellers.py
+++ models/tellers.py
@@ -358,15 +358,24 @@ class Tellers(models.Model):
                 template_id = self.env.ref('spot_check.email_template_create_teller_request').id
                 template =  self.env['mail.template'].browse(template_id)
                 template.send_mail(req.id,force_send=True)
+    ''''
+    @api.constrains('teller_id')
+    def _checkbranchspotcheck(self):
+        pending_conf = self.env['spot_check.teller'].search([('state', 'in', ['ongoing'])])
+        for res in pending_conf:
+            if  res.teller_id.id == self.teller_id.id and res.state =='ongoing':
+                raise exceptions.ValidationError(f"Hello {res.partner_id.name},  {res.branch_id.branch_name} still has a pending spot check confirmantion  of {res.grand_total_ugx:,.2f} UGX created on {res.created_on} by {res.created_by.name} . Kindly inform Teller {res.teller_id.name} to cosent all the spot checks before you proceed. For any more assistance please contact operations ")
+
+    '''
     
-    '''@api.one
+    @api.one
     @api.constrains('branch_id')
     def _checkbranchspotcheck(self):
         pending_conf = self.env['spot_check.teller'].search([('state', 'in', ['ongoing'])])
         for res in pending_conf:
-            if  res.branch_id.id == self.branch_id.id and res.state =='ongoing' and res.id is not self.id:
+            if  res.branch_id.id == self.branch_id.id and res.state =='ongoing' and res.unique_field is not self.unique_field:
                 raise exceptions.ValidationError(f"Hello {res.partner_id.name},  {res.branch_id.branch_name} still has a pending spot check confirmantion  of {res.grand_total_ugx:,.2f} UGX created on {res.created_on} by {res.created_by.name} . Kindly inform Teller {res.teller_id.name} to cosent all the spot checks before you proceed. For any more assistance please contact operations ")
-    '''
+    
     '''
     @api.one
     @api.constrains('created_on')
